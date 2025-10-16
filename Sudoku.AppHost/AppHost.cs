0fd4db67dd3d26891e9a119b887d82ee1fc185f7
oliver wasner <oli.wasner@hotmail.com>
2025-10-16 23:45:13 +0200

    added menu item to launch scalar docs

--- Sudoku.AppHost/AppHost.cs
+++ Sudoku.AppHost/AppHost.cs
@@ -1,7 +1,24 @@
+using System.Buffers.Text;
+using System.Diagnostics;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.Sudoku_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
+
+apiService.WithCommand("scalar", "Sclar UI", executeCommand: async _ => {
+    try 
+    { 
+        var enpoint = apiService.GetEndpoint("https").Url; 
+        var url = $"{enpoint}/scalar/v1";
+        Process.Start(new ProcessStartInfo(url) { UseShellExecute = true }); 
+        return new ExecuteCommandResult { Success = true };
+    }
+    catch (Exception ex) 
+    { 
+        return new ExecuteCommandResult { Success = false, ErrorMessage = ex.Message };
+    } 
+});
 
 var client = builder.AddNpmApp("client", "../client")
     .WithReference(apiService)
